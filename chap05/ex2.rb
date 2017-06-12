a3ad2eeac250d20dbf337bc2448dce502cea39cb
Mutsa Munyawiri <mutsa.evelyn@hotmail.com>
2017-06-12 20:26:37 +0100

    Chap05 exercise 2 - Bigger, better favourite number.

---
+++ chap05/ex2.rb
@@ -0,0 +1,6 @@
+#!/Users/mutsamunyawiri/.rvm/rubies/ruby-2.4.0/bin/ruby
+
+puts 'Hello there, what is your favourite number?'
+f_num = gets.chomp.to_i
+big_n_better_num = f_num + 1
+puts "#{f_num} is great, however, I think #{big_n_better_num} would be a bigger and better favourite number!"
