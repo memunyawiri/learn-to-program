af2ba9301f75fc78e2eeba96d76d37d653fff11d
Mutsa Munyawiri <mutsa.evelyn@hotmail.com>
2017-06-12 16:16:30 +0100

    Chapter three run-through, no exercises

---
+++ chap03/letters.rb
@@ -0,0 +1,30 @@
+#!/Users/mutsamunyawiri/.rvm/rubies/ruby-2.4.0/bin/ruby
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts ''
+
+puts 'I like ' + 'apple pie.'
+puts ''
+
+puts 'blink ' * 4
+
+puts ''
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts ''
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+puts ''
+
+puts 'You\'re swell!'
+puts ''
+
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
