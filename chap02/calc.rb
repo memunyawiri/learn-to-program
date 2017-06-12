9503b0e7a88381ded695657a50e67c59a948e618
Mutsa Munyawiri <mutsa.evelyn@hotmail.com>
2017-06-12 14:18:51 +0100

    Main calc doc with all exs

---
+++ chap02/calc.rb
@@ -0,0 +1,32 @@
+#!/Users/mutsamunyawiri/.rvm/rubies/ruby-2.4.0/bin/ruby
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts ""
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts ""
+
+puts 5* (12-8) + -15
+puts 98 + (59872 / (13*8)) * -51
+
+#Challenges - A Few Things to try
+puts ""
+
+#1. Hours in a year
+puts 24*365
+
+#2. Minutes in a decade
+puts 365 * (24 * 60) * 10
+
+#3. My age in seconds (24)
+puts 365 * (24 * (60 * 60)) * 24
+
+#4. Authors age from seconds (1,160+6)
+puts 1160000000 / (365 * (24 * 3600))
