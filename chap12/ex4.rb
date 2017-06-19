e9aaa7946e0f2b41dbf8ccc5dd11360be28f6a15
Mutsa Munyawiri <mutsa.evelyn@hotmail.com>
2017-06-19 09:20:15 +0100

    Chap12 Ex4 - Birthday Helper

---
+++ chap12/ex4.rb
@@ -0,0 +1,27 @@
+#!/Users/mutsamunyawiri/.rvm/rubies/ruby-2.4.0/bin/ruby
+
+birth_dates = {}
+File.read('birthdates.txt').each_line do |line|
+  line = line.chomp
+
+  first_comma = 0
+  while line[first_comma] != ',' &&
+    first_comma < line.length
+    first_comma = first_comma + 1
+  end
+
+  name = line[0..(first_comma - 1)]
+  date = line[-12..-1]
+
+  birth_dates[name] = date
+end
+
+puts 'Whose birthday are you looking for?'
+name = gets.chomp
+date = birth_dates[name]
+
+if date == nil
+  puts "Oooh, I can't find that one..."
+else
+  puts date[0..5]
+end
