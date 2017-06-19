fa5cb197036cd3c823d5f6f8f76d4b34ea40c7f9
Mutsa Munyawiri <mutsa.evelyn@hotmail.com>
2017-06-19 19:37:21 +0100

    Chap14 Ex2 - Grandfather Clock

---
+++ chap14/ex2.rb
@@ -0,0 +1,20 @@
+#!/Users/mutsamunyawiri/.rvm/rubies/ruby-2.4.0/bin/ruby
+
+def grandfather_clock &block
+  hour = Time.new.hour
+
+  if hour >= 13
+    hour = hour - 12
+  end
+
+  if hour == 0
+    hour = 12
+  end
+
+  hour.times do
+    block.call
+  end
+end
+grandfather_clock do
+  puts 'DONG!!!' #=> should be 7 of these strings for current time
+end
